c92c3790c6ecd7d9c2051d7bd4b38b77333b1f17
unknown <42289375+TommyXR@users.noreply.github.com>
2018-12-09 15:52:59 -0500

    ajout de getCalendar

--- index.js
+++ index.js
@@ -107,6 +107,9 @@ var getIndex = function (replacements) {
 
 // --- À compléter ---
 
+// ---- Général ----
+
+
 var listeSondages = [];
 
 var mois = [
@@ -116,13 +119,68 @@ var mois = [
 
 var MILLIS_PAR_JOUR = (24 * 60 * 60 * 1000);
 
+
+//fonctions générales 
+
+
+//Retourne la position dans la liste de sondages
+//du sondage demandé
+function findSondage(sondageId) {
+	
+	//On cherche l'id du sondage parmi notre liste de sondages
+	for(var i = 0; i < listeSondages.length; i++) {
+		
+		if( listeSondages[i].id == sondageId ) return listeSondages[i];
+		
+	}
+	
+	console.log("Sondage " + sondageId + " introuvable dans la liste!");
+	return -1;
+
+}
+
 // Retourne le texte HTML à afficher à l'utilisateur pour répondre au
 // sondage demandé.
 //
 // Doit retourner false si le calendrier demandé n'existe pas
 var getCalendar = function (sondageId) {
     // TODO
-    return 'Calendrier <b>' + sondageId + '</b> (TODO)';
+	var sondage = findSondage(sondageId);
+	
+	var defaultDoc = readFile("template/calendar.html");
+	
+	//On recréé les dates car elles ont mal été enregistrées
+	//(Va savoir pourquoi)
+	var dateDebut = new Date(sondage.dateDebut);
+	var dateFin= new Date(sondage.dateFin);
+	
+	var nbJours = (dateFin-dateDebut)/MILLIS_PAR_JOUR;
+	
+	//On créé notre table en suivant le modèle donné dans l'énoncé du TP
+	var table = "<table id = \"calendrier\" \n" +
+							"onmousedown = \"onClick(event)\" \n" +
+							"onmouseover = \"onMove(event)\" \n" +
+							"data-nbjours = \"" +
+							nbJours +
+							"\" data-nbheures = \"" + 
+							(+sondage.heureFin - +sondage.heureDebut) +
+							"\">"
+	
+	for(var i = 0; i < nbJours; i++) {
+		
+		table += "<tr> <th> </th>";
+		
+		
+							
+	console.log(+sondage.heureFin - +sondage.heureDebut);
+							
+	
+	
+	defaultDoc = defaultDoc.replace( /\{\{titre\}\}/g , sondage.titre).replace(
+	/\{\{url\}\}/g, " http://localhost:1337/" + sondage.id);
+	
+	
+    return 'Calendrier <b>' + sondageId + '</b> (TODO)' + defaultDoc;
 };
 
 // Retourne le texte HTML à afficher à l'utilisateur pour voir les
@@ -143,7 +201,7 @@ var creerSondage = function(titre, id, dateDebut, dateFin, heureDebut, heureFin)
 	
 	//On commence par vérifier les informations
 	
-	if ( !RegExp('^[a-zA-Z0-9\-]+$').test(id) ) return false;
+	if ( !(/^[a-zA-Z0-9\-]+$/.test(id) ) ) return false;
 	
 	if ( +heureDebut > +heureFin ) return false;
 	
