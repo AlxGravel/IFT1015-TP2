71b4c514c6bd13da358163332aaa82fdfe6d9d93
unknown <42289375+TommyXR@users.noreply.github.com>
2018-12-12 17:29:36 -0500

    v2

--- README.txt
+++ README.txt
@@ -1,9 +1,4 @@
 ﻿=====MESSAGE D'ERREUR=====
-Pour que le message d'erreur soit affiché correctement, on a du tester
-les différentes erreurs et renvoyer des résultats différents selon
-l'erreur dans la fonction creerSondage. On a donc remplacé la ligne
-if(resultat.exists) par if(resultat.exists > 0) dans la fonction 
-indexQuery.
 
 ====FAILLES DE SÉCURITÉS====
 
@@ -12,5 +7,5 @@ indexQuery.
 On a corrigé le cross-site scripting en créant une fonction
 parseUserInput qui filtre les <, > et " entrées par un utilisateur
 dans tous les champs où il peut entrer du texte (soit le titre
-et le nom du sondage, et le nom du participant). Lorsqu'on 
+et le nom du sondage, et le nom du participant).
 
--- index.js
+++ index.js
@@ -435,18 +435,34 @@ var creerSondage = function(titre, id, dateDebut, dateFin, heureDebut, heureFin)
 	
 	//On commence par vérifier les informations
 	
-	if ( !(/^[a-zA-Z0-9\-]+$/.test(id) ) ) return false;
+	var b = document.getElementById("error");
 	
-	if ( +heureDebut > +heureFin ) return false;
+	b.innerHTML = "";
 	
-	if ( dateFin - dateDebut < 0 ) return false;
+	if ( !(/^[a-zA-Z0-9\-]+$/.test(id) ) ) {
+		b += "Veuillez entrer un id valide\n";
+		return false;
+	}
 	
-	if ( dateFin - dateDebut > 30*MILLIS_PAR_JOUR ) return false;
+	if ( +heureDebut > +heureFin ) {
+		b += "L'heure de fin doit être plus grande que l'heure de début!\n";
+		return false;
+	}
+	
+	if ( dateFin - dateDebut < 0 ) {
+		b += "La date de fin doit être plus grande que la date de début!\n";
+		return false;
+	}
+	
+	if ( dateFin - dateDebut > 30*MILLIS_PAR_JOUR ) {
+		b += "L'étendue maximale d'un sondage est de 30 jours";
+		return false;
+	}
 	
 	
 	//On veut ensuite créer le sondage et l'ajouter à notre liste.
 	var sondage = {
-	    "titre":parseUserInput(titre), "id":id,
+	    "titre":parseUserInput(titre), "id":parseUserInput(id),
 	    "dateDebut":new Date(dateDebut), "dateFin":new Date(dateFin),
 	    "heureDebut":heureDebut, "heureFin":heureFin,
 	    "participants": []
