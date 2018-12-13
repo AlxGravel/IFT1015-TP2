fce76af9cce5bec33afce68da12fa9b06a10fbbe
AlxGravel <45745067+AlxGravel@users.noreply.github.com>
2018-12-12 20:34:58 -0500

    Update ajouterParticipant.js

--- fctsIndex/ajouterParticipant.js
+++ fctsIndex/ajouterParticipant.js
@@ -1,8 +1,6 @@
 var ajouterParticipant = function(sondageId, nom, disponibilites) {
-    // TODO
-    var sondage = findSondage(sondageId);
 
-    sondage.participants.push({"nom": parseUserInput(nom), "disponibilites": disponibilites});
-	
-	return;
+    findSondage(sondageId).participants.push({"nom": parseUserInput(nom),
+                                              "disponibilites": disponibilites
+                                             });
 };
