d9f546f22859fe36fe4dd3858e4eb2fc447825e4
AlxGravel <45745067+AlxGravel@users.noreply.github.com>
2018-12-12 20:36:51 -0500

    Update ajouterParticipant.js

--- fctsIndex/ajouterParticipant.js
+++ fctsIndex/ajouterParticipant.js
@@ -1,3 +1,4 @@
+//enregistre un nouveau participant pour le sondage "sondageId"
 var ajouterParticipant = function(sondageId, nom, disponibilites) {
 
     findSondage(sondageId).participants.push({"nom": parseUserInput(nom),
