10a3e3fe22fc4213f2617c2d973f96b5a986a9ab
AlxGravel <45745067+AlxGravel@users.noreply.github.com>
2018-12-12 20:54:25 -0500

    Update genColor.js

--- fctsIndex/genColor.js
+++ fctsIndex/genColor.js
@@ -1,32 +1,29 @@
-
+//converti une couleur du format 0à1 vers hexadécimal HTML
 var colorToHexa = function(color){
-    color = Math.floor(color * 255);
-    color = color.toString(16);
-    if(color.length == 1)
-	color = "0" + color;
+    color = Math.floor(color * 255);//conversion decimale
+    color = color.toString(16);//conversion hexadecimale
+
+    if(color.length == 1)//ajout d'un 0 si nécessaire
+        color = "0" + color;
 
     return color;
-
 };
 
 // Génère la `i`ème couleur parmi un nombre total `total` au format
 // hexadécimal HTML
-//
-// Notez que pour un grand nombre de couleurs (ex.: 250), générer
-// toutes les couleurs et les afficher devrait donner un joli dégradé qui
-// commence en rouge, qui passe par toutes les autres couleurs et qui
-// revient à rouge.
 var genColor = function(i, nbTotal) {
-    // TODO
-    
+
+    //application de la formule de l'énoncé
     var teinte = (i/nbTotal) * 360;
     var h = teinte/60;
     var c = 0.7;
     var x = c*(1-Math.abs(h%2-1));
 
+    //conversion au format hexadécimal HTML
     var hexaC = colorToHexa(c);
     var hexaX = colorToHexa(x);
 
+    //retour dans l'ordre fourni dans l'énoncé
     switch(Math.floor(h)){
         case 0:return "#" + hexaC + hexaX + "00";
         case 1:return "#" + hexaX + hexaC + "00";
@@ -34,6 +31,6 @@ var genColor = function(i, nbTotal) {
         case 3:return "#" + "00" + hexaX + hexaC;
         case 4:return "#" + hexaX + "00" + hexaC;
         case 5:return "#" + hexaC + "00" + hexaX;
-	default:return "#000000";
+        default:return "#000000";
     }
 };
