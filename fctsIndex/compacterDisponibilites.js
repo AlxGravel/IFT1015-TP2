03ffd9174de1abd68b487319796a0f0930ca73af
AlxGravel <45745067+AlxGravel@users.noreply.github.com>
2018-12-12 20:44:23 -0500

    Update compacterDisponibilites.js

--- fctsIndex/compacterDisponibilites.js
+++ fctsIndex/compacterDisponibilites.js
@@ -1,4 +1,6 @@
-
+//retourne une lecture du tableau de disponibilitées
+//sous forme de chaine de caractères binaire
+//1 pour disponible et 0 pour non disponible
 var compacterDisponibilites = function() {
 
     var cal = document.getElementById("calendrier");
@@ -6,17 +8,15 @@ var compacterDisponibilites = function() {
     var nbJours = cal.dataset.nbjours;
 
     var dispo = "";
-    
+
     for( var i = 0; i < nbHeures; i++) {
-		
-		for(var j = 0; j < nbJours; j++){
-			if(document.getElementById(j+"-"+i).innerHTML == "")
-			dispo += "0";
-			else
-			dispo += "1";
-		}
-		
-	}
+        for(var j = 0; j < nbJours; j++){
+            if(document.getElementById(j+"-"+i).innerHTML == "")
+                dispo += "0";
+            else
+                dispo += "1";
+        }
+    }
 
     return dispo;
 };
