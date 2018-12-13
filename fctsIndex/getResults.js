3e56a6c95a2ece09f9279f6bd46d2b5585b6ac36
AlxGravel <45745067+AlxGravel@users.noreply.github.com>
2018-12-13 15:11:10 -0500

    Update getResults.js

--- fctsIndex/getResults.js
+++ fctsIndex/getResults.js
@@ -1,4 +1,3 @@
-
 // Retourne le texte HTML à afficher à l'utilisateur pour voir les
 // résultats du sondage demandé
 //
@@ -8,11 +7,14 @@ var getResults = function (sondageId) {
     var resultsTmp = readFile("template/results.html");
 
     var sondage = findSondage(sondageId);
-	
-    var nbJours = (sondage.dateFin.getTime() - sondage.dateDebut.getTime())/MILLIS_PAR_JOUR+1;
+
+    var nbJours = (sondage.dateFin.getTime()
+		   - sondage.dateDebut.getTime())
+                  /MILLIS_PAR_JOUR+1;
+
     var nbHeures = +sondage.heureFin - +sondage.heureDebut + 1;
 
- 
+
 
     //assignation des couleurs aux participants
     var colorTab = [];
@@ -20,7 +22,7 @@ var getResults = function (sondageId) {
     for(var i = 0; i < sondage.participants.length; i++){
 		colorTab.push(genColor(i, sondage.participants.length))
     }
-    
+
     //creation du tableau
     //TODO: max et min
 	var table = writeResults(sondage, colorTab);
@@ -30,138 +32,144 @@ var getResults = function (sondageId) {
 
     //usage de regex approuvé par un auxiliaire de cours sur Studium
     return resultsTmp.replace(/\{\{titre\}\}/g, sondage.titre)
-	            .replace(/\{\{url\}\}/g, 
+	            .replace(/\{\{url\}\}/g,
 				"http://localhost:1337/" + sondage.id)
 	            .replace(/\{\{table\}\}/g, table)
 	            .replace(/\{\{legende\}\}/g, legende);
 };
 
-//*******************************************************************************
+//*************************************************************************
 
 //Fonctions auxiliaires
 
 
 function writeResults(sondage, colorTab) {
-	
-    var nbJours = (sondage.dateFin.getTime() - sondage.dateDebut.getTime())/MILLIS_PAR_JOUR+1;
+
+    var nbJours = (sondage.dateFin.getTime()
+		   - sondage.dateDebut.getTime())
+                  /MILLIS_PAR_JOUR+1;
     var nbHeures = +sondage.heureFin - +sondage.heureDebut;
-	
-	var participantsMax = getMax(sondage);
-	
-	var participantsMin = getMin(sondage);
-	
-	var table = "<table>" + "<tr>" + "<th></th>";
-	
-	for(var i = 0; i < nbJours; i++) {
-		
-		var jour = addDays(sondage.dateDebut, i+1);
-		
-		table += "<th>" + jour.getDate() + " " + mois[jour.getMonth()] + "</th>";
-		
-	}
-	
-	table += "</tr>";
-	
-	for(var i = 0; i < nbHeures; i++) {
-		
-		table += "<tr>" + "<th>" + (+sondage.heureDebut + i) +"h</th>";
-		
-		for(var j = 0; j < nbJours; j++) {
-			
-			var isMax = false, isMin = false;
-			var nb = 0;
-			
-			for(var k = 0; k < sondage.participants.length; k++){
-				
-				if(sondage.participants[k].disponibilites.charAt(i*nbJours+j) == '1')
-					nb++;
-			}
-			
-			if(nb == participantsMax) isMax = true;
-			if(nb == participantsMin) isMin = true;
-			
-			
-			table += "<td" + (isMax?" class=\"max\"":"") + (isMin?" class=\"min\"":"") + ">";
-			
-			//i*nbJours+j est la position linéaire dans le tableau
-			for(var k = 0; k < sondage.participants.length; k++){
-				
-				if(sondage.participants[k].disponibilites.charAt(i*nbJours+j) == '1'){
-				
-					table += "<span style=\"background-color:"+ colorTab[k] +
-					";color:" + colorTab[k] + "\">.</span>";
-					   
-				}
-				
-			}
-			
-			table+= "</td>";
-			
+
+    var participantsMax = getMax(sondage);
+
+    var participantsMin = getMin(sondage);
+
+    var table = "<table>" + "<tr>" + "<th></th>";
+
+    for(var i = 0; i < nbJours; i++) {
+
+	var jour = addDays(sondage.dateDebut, i+1);
+
+	table += "<th>" + jour.getDate() + " "
+	    + mois[jour.getMonth()] + "</th>";
+
+    }
+
+    table += "</tr>";
+
+    for(var i = 0; i < nbHeures; i++) {
+
+	table += "<tr>" + "<th>" + (+sondage.heureDebut + i) +"h</th>";
+
+	for(var j = 0; j < nbJours; j++) {
+
+	    var isMax = false, isMin = false;
+	    var nb = 0;
+
+	    for(var k = 0; k < sondage.participants.length; k++){
+
+		if(sondage.participants[k].disponibilites.
+		   charAt(i*nbJours+j) == '1')
+
+		    nb++;
+	    }
+
+	    if(nb == participantsMax) isMax = true;
+	    if(nb == participantsMin) isMin = true;
+
+
+	    table += "<td" + (isMax?" class=\"max\"":"")
+		+ (isMin?" class=\"min\"":"") + ">";
+
+	    //i*nbJours+j est la position linéaire dans le tableau
+	    for(var k = 0; k < sondage.participants.length; k++){
+
+		if(sondage.participants[k].disponibilites
+		   .charAt(i*nbJours+j) == '1'){
+
+		    table += "<span style=\"background-color:"+ colorTab[k] +
+			";color:" + colorTab[k] + "\">.</span>";
+
 		}
-		
-		table += "</tr>";
+
+	    }
+
+	    table+= "</td>";
+
 	}
-	
-	table += "</table>";
-	
-	return table;
-	
-	console.log("min = " + participantsMin + "\nmax = " + participantsMax);
-	
+
+	table += "</tr>";
+    }
+
+    table += "</table>";
+
+    return table;
+
+    console.log("min = " + participantsMin + "\nmax = " + participantsMax);
+
 }
 
 function writeLegend(sondage, colorTab) {
-	
-	var legende = "";
-	
-	for(var i = 0; i < sondage.participants.length; i++){
-		legende += "<li style=\"background-color:" + colorTab[i] +
-			"\">" + sondage.participants[i].nom + "</li>";
+
+    var legende = "";
+
+    for(var i = 0; i < sondage.participants.length; i++){
+	legende += "<li style=\"background-color:" + colorTab[i] +
+	    "\">" + sondage.participants[i].nom + "</li>";
 
     }
-	
-	return legende;
-	
+
+    return legende;
+
 }
 
 function getMax(sondage) {
-	
-	var max = 0;
-	
-	for(var i = 0; i < sondage.participants[0].disponibilites.length; i++) {
-		
-		var nb = 0;
-		for(var j = 0; j < sondage.participants.length; j++) {
-			
-			if( sondage.participants[j].disponibilites.charAt(i) == 1) ++nb;
-			
-		}
-		
-		if( nb > max ) max = nb;
-		
+
+    var max = 0;
+
+    for(var i = 0; i < sondage.participants[0].disponibilites.length; i++) {
+
+	var nb = 0;
+	for(var j = 0; j < sondage.participants.length; j++) {
+
+	    if( sondage.participants[j].disponibilites.charAt(i) == 1) ++nb;
+
 	}
-	
-	return max;
+
+	if( nb > max ) max = nb;
+
+    }
+
+    return max;
 }
 
 
 function getMin(sondage) {
-			
-	var min = 1e10;
-	
-	for(var i = 0; i < sondage.participants[0].disponibilites.length; i++) {
-		
-		var nb = 0;
-		for(var j = 0; j < sondage.participants.length; j++) {
-			
-			if( sondage.participants[j].disponibilites.charAt(i) == 1) ++nb;
-			
-		}
-		
-		if( nb < min ) min = nb;
-		
+
+    var min = 1e10;
+
+    for(var i = 0; i < sondage.participants[0].disponibilites.length; i++) {
+
+	var nb = 0;
+	for(var j = 0; j < sondage.participants.length; j++) {
+
+	    if( sondage.participants[j].disponibilites.charAt(i) == 1) ++nb;
+
 	}
-	
+
+	if( nb < min ) min = nb;
+
+	}
+
 	return min;
 }
-
